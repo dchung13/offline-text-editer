1c61f04e3c9f70540762dae07c8fba504d7bc8dd
David C <dchung20315@gmail.com>
2023-05-24 19:20:49 -0700

    fixed service worker

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = await store.put(content);
+  const request = await store.add(content);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
